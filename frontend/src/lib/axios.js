f8429b11a8cc0d55568dd1b51ec39e0e0077479b
Harshilll29 <harshiljoshi525@gmail.com>
2025-06-30 14:46:56 +0530

    first commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://nexora-6ld3.onrender.com/",
+    baseURL: "https://nexora-6ld3.onrender.com/api",
     withCredentials: true, //send cookies with the req
 });
