e816f17f88512c8a6023d6a3a115df119772bed2
Harshilll29 <harshiljoshi525@gmail.com>
2025-06-30 14:35:20 +0530

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://nexora-1.vercel.app/login",
     credentials: true
 }));
 app.use(cookieParser());
